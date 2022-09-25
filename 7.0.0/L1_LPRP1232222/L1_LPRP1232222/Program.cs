cf3c4c2bd787eb67806090037d15b850ae7e0d7e
luluro1232222 <111794940+luluro1232222@users.noreply.github.com>
2022-09-25 02:10:42 +0200

    Add files via upload

---
+++ 7.0.0/L1_LPRP1232222/L1_LPRP1232222/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese su nombre: ");
+        string Nombre = Console.ReadLine();
+        
+        Console.WriteLine("Hola Mundo");
+        Console.WriteLine("Soy "+ Nombre); 
+        /* Console.WriteLine escribe en diferentes líneas y Console.Write escibe el texto de largo o en la misma línea*/ 
+
+        Console.Write("Hola Mundo");
+        Console.Write("Soy "+ Nombre);
+        Console.ReadKey();
+    }
+}
