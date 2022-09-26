49f722387c0c869386eb522618d9e5a35bb43657
luluro1232222 <111794940+luluro1232222@users.noreply.github.com>
2022-09-26 21:23:43 +0200

    Add files via upload

---
+++ 8.0.0/L5LPRP1232222/L5LPRP1232222/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int  numeroEntero = 0;
+
+Console.WriteLine("EJERCICIO 1");
+Console.WriteLine("Número Entero");
+
+numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+if (numeroEntero >= 1)
+{
+    Console.WriteLine("Número positivo");
+}
+if (numeroEntero <= -1)
+{
+    Console.WriteLine("Numero negativo");
+}
+if (numeroEntero == 0)
+{
+    Console.WriteLine("El número que ingresó es cero");
+}
