a95d22f1925fa8e7068edc9d7c6e7e9b0eae31c7
luluro1232222 <111794940+luluro1232222@users.noreply.github.com>
2022-11-13 22:36:17 -0600

    Add files via upload

---
+++ 10.0.0 Teorica/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese primer numero");
+        num1(Convert.ToInt32(Console.ReadLine()));
+        numero1 = Console.ReadLine();
+        Console.WriteLine("Ingrese segundo numero");
+        Console.WriteLine("Ingrese resultado");
+    }
+
+    public int num1(int numero1)
+    {
+    numero1 = Numero1
+    }
+    
+    public string Suma()
+    {
+        int Numero1;
+        int numero2;
+        int suma = numero1 + numero2;
+        
+        if (suma == sumaIngresada)
+        {
+            Console.WriteLine("Verdadera");
+        }
+        else
+        {
+            Console.WriteLine("Vuelva a intentar");
+        }       
+    }
+
+    public string MostarRespuesta()
+    {
+        return Suma();
+    }
+
+    
+}
