6399f787b39877862903f9a2a88f828e508a9458
luluro1232222 <111794940+luluro1232222@users.noreply.github.com>
2022-11-13 22:36:52 -0600

    Add files via upload

---
+++ 11.0.0 Teorica/reto2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] niveles = new int[5];
+        int[] niños = new int[5];
+        int[] mujeres = new int[5];
+        int suma = 0;
+        int nivelmenor = 0;
+        int respuesta = 0;
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Ingrese la cantidad de niños por nivel ");
+            niños[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de mujeres por nivel ");
+            mujeres[i] = int.Parse(Console.ReadLine()); 
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("nivel"+(i+1));
+            Console.WriteLine(niños[i] + mujeres[i]);
+            
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+          suma+= niños[i] + mujeres[i];
+            Console.WriteLine("en el nivel"+ (i+1) +"estan"+suma+ "personas");
+        }
+
+
+        for (int i = 0; i <5 ; i++)
+        {
+            if(nivelmenor< niños[i])
+                {
+                nivelmenor = niños[i];
+                respuesta = i + 1;
+            }
+           
+        }
+        Console.WriteLine("el nivel con mas niños es "+ nivelmenor);
+    }
+}
---
+++ 11.0.0 Teorica/reto3/reto3/reto3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[] niveles = new int[5];
+        int[] niños = new int[5];
+        int[] adultos = new int[5];
+        string[] responsable = new string[5];
+        int suma = 0;
+        int cantidadpersonas = 0;
+        int respuesta = 0;
+        string encargadomayor="";
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Ingrese la persona responsable por nivel ");
+            responsable[i] = Console.ReadLine();
+            Console.WriteLine("Ingrese la cantidad de niños por nivel ");
+            niños[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de mujeres por nivel ");
+            adultos[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("nivel" + (i + 1));
+            Console.WriteLine(niños[i] + adultos[i]+"con responsable"+ responsable[i]);
+
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            suma += niños[i] + adultos[i];
+            Console.WriteLine("en el nivel" + (i + 1) + "estan" + suma + "personas");
+        }
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (cantidadpersonas < niños[i]+ adultos[i])
+            {
+                cantidadpersonas = niños[i] + adultos[i];
+                respuesta = i + 1;
+                encargadomayor = responsable[i];
+
+            }
+
+        }
+        Console.WriteLine("el responsable del nivel con mas personas es " + encargadomayor);
+    }
+}
